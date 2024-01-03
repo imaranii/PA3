076ebcd943e8cdaf18fd43a94f08b248b8f463e9
imaranii <145257750+imaranii@users.noreply.github.com>
2024-01-03 18:33:24 +0100

    Create BSTreeDicet.h

---
+++ BSTreeDicet.h
@@ -0,0 +1,50 @@
+#ifndef BSTREEDICT_H
+#define BSTREEDICT_H
+
+#include <ostream>
+#include <stdexcept>
+#include "Dict.h"
+#include "BSTree.h"
+#include "TableEntry.h"
+
+template <typename V>
+class BSTreeDict: public Dict<V> {
+
+    private:
+        BSTree<TableEntry<V>>* tree;
+
+    public:
+        BSTreeDict() : tree(new BSTree<TableEntry<V>>()) {}
+
+        ~BSTreeDict(){
+            delete tree;
+        }
+
+	friend std::ostream& operator<<(std::ostream &out, const BSTreeDict<V> &bs){
+		out << bs.tree;
+		return out;
+	}
+
+        V operator[](std::string key){
+            return search(key);
+        }
+
+        void insert(std::string key, V value) override final{
+            TableEntry<V>* entry = new TableEntry<V>(key, value);
+            tree->insert(*entry);
+        }
+
+        V search(std::string key) override final{
+            return tree->search(key).value;
+        }
+
+        V remove(std::string key) override final{
+            return tree->remove(key).value;
+        }
+
+        int entries(){
+            return tree->size();
+        }
+};
+
+#endif
